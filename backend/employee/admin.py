5086998dd7ab84c10d554f12ea56f16033110fcd
jamil-baifam <jamil.lutaaya@baifamgroup.co.ug>
2025-06-26 17:51:24 +0300

    overtime hours

--- backend/employee/admin.py
+++ backend/employee/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Employee, WorkType, EmployeeType
+from .models import Employee, WorkType, EmployeeType, EmployeeAttendance
 
 admin.site.register(Employee)
 admin.site.register(WorkType)
 admin.site.register(EmployeeType)
+admin.site.register(EmployeeAttendance)
