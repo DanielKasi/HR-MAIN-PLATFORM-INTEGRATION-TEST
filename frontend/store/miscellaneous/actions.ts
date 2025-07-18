94ab47178b842a5e842763bcd60c9dc1a8bdb9e6
DanielKasi <daniel.kasi@baifamgroup.co.ug>
2025-07-18 12:58:37 +0300

    Changed dashboard on HR

--- frontend/store/miscellaneous/actions.ts
+++ frontend/store/miscellaneous/actions.ts
@@ -6,6 +6,16 @@ import {MISC_ACTION_TYPES} from "./types";
 
 type ToggleSideBar = Action<MISC_ACTION_TYPES.TOGGLE_SIDEBAR>;
 
-export type MiscAction = ToggleSideBar;
+export type MiscAction = ToggleSideBar|OpenSideBar| CloseSideBar
 
 export const toggleSideBarAction = () => createAction(MISC_ACTION_TYPES.TOGGLE_SIDEBAR);
+
+type OpenSideBar = Action<MISC_ACTION_TYPES.OPEN_SIDE_BAR>
+type CloseSideBar = Action<MISC_ACTION_TYPES.CLOSE_SIDE_BAR>
+
+
+
+export const openSideBar = ():OpenSideBar => createAction(MISC_ACTION_TYPES.OPEN_SIDE_BAR)
+
+export const closeSideBar = ():CloseSideBar => createAction(MISC_ACTION_TYPES.CLOSE_SIDE_BAR)
+
