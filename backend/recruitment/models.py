4a89e4300f048351615d7894f3b92e1db0bba9f2
DanielKasi <daniel.kasi@baifamgroup.co.ug>
2025-07-29 11:49:38 +0300

    Minor fix on job positions approval steps

--- backend/recruitment/models.py
+++ backend/recruitment/models.py
@@ -49,8 +49,8 @@ class JobPosition(models.Model):
         return f"{self.name}"
 
     def activate_job_position(self):
-        if self.job_position_status != "inactive":
-            raise ValidationError("Only inactive job positions can be activated.")
+        # if self.job_position_status != "inactive":
+        #     raise ValidationError("Only inactive job positions can be activated.")
 
         self.job_position_status == "active"
         self.save()
