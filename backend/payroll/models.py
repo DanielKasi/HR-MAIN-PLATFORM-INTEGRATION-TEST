9452c0f209ded3b54d3e39605641653c71a335e5
SamuelAtBaifam <samuel.senyonga@baifamgroup.co.ug>
2025-08-07 16:06:40 +0300

    Remove TaxRule and TaxType models from payroll module

--- backend/payroll/models.py
+++ backend/payroll/models.py
@@ -370,39 +370,38 @@ class PayrollPeriod(models.Model):
         ordering = ["-start_date"]
 
 
-class TaxType(models.Model):
-    """Defines a general type of tax, linked directly to an institution
-    e.g, PAYE - Uganda or NSSF
-    """
+# class TaxType(models.Model):
+#     """Defines a general type of tax, linked directly to an institution
+#     e.g, PAYE - Uganda or NSSF
+#     """
 
-    institution = models.ForeignKey(
-        'institution.Institution',
-        on_delete=models.CASCADE,
-        related_name='tax_types',
-        help_text='The institution this tax type belongs to.'
-    )
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
-    is_active = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+#     institution = models.ForeignKey(
+#         'institution.Institution',
+#         on_delete=models.CASCADE,
+#         related_name='tax_types',
+#         help_text='The institution this tax type belongs to.'
+#     )
+#     name = models.CharField(max_length=100)
+#     description = models.TextField(blank=True, null=True)
+#     is_active = models.BooleanField(default=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     update_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return f"{self.name} ({self.institution.institution_name})"
-    
-    class Meta:
-        unique_together = ['institution', 'name']
-        ordering = ['name']
-    
-class TaxRuleCalculationChoices(models.TextChoices):
-    ('fixed_percentage', 'Fixed_Percentage'),
-    ('tiered_brackets', 'Tiered_Brackets')
+#     def __str__(self):
+#         return f"{self.name} ({self.institution.institution_name})"
+
+#     class Meta:
+#         unique_together = ['institution', 'name']
+#         ordering = ['name']
+
+# class TaxRuleCalculationChoices(models.TextChoices):
+#     ('fixed_percentage', 'Fixed_Percentage'),
+#     ('tiered_brackets', 'Tiered_Brackets')
 
 
-class TaxRule(models.Model):
-    """Defines a tax rule linked to a specific TypeTax
-    """
-    
+# class TaxRule(models.Model):
+#     """Defines a tax rule linked to a specific TypeTax
+#     """
 
 
 class Payslip(models.Model):
