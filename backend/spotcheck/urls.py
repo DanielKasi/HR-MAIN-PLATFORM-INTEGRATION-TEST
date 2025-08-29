1da9cf1bd1290561a044135ee6f1004859e73c8d
phillip mugisa <phillip.mugisa@baifamgroup.co.ug>
2025-08-29 17:41:57 +0300

    implemented spot check endpoints

--- backend/spotcheck/urls.py
+++ backend/spotcheck/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path("employee/<int:employee_id>/setting/details/", SpotCheckViews.EmployeeStopCheckSettingDetailView.as_view(), name='branch-spotchecksetting-detail'),
     path("employee/<int:employee_id>/setting/update/", SpotCheckViews.EmployeeStopCheckSettingUpdateView.as_view(), name='employee-spotchecksetting-update'),
 
-    path("employee/<int:spotcheck_id>/spotcheck/create/", SpotCheckViews.EmployeeStopCheckCreateView.as_view(), name='employee-spotcheck-create'),
-    path("employee/<int:spotcheck_id>/spotcheck/details/", SpotCheckViews.EmployeeStopCheckDetailView.as_view(), name='employee-spotcheck-detail'),
-    path("employee/<int:spotcheck_id>/spotcheck/checkin/", SpotCheckViews.EmployeeStopCheckUpdateView.as_view(), name='employee-spotcheck-update'),
+    path("<int:spotcheck_id>/spotcheck/create/", SpotCheckViews.EmployeeStopCheckCreateView.as_view(), name='employee-spotcheck-create'),
+    path("<int:spotcheck_id>/spotcheck/details/", SpotCheckViews.EmployeeStopCheckDetailView.as_view(), name='employee-spotcheck-detail'),
+    path("<int:spotcheck_id>/spotcheck/checkin/", SpotCheckViews.EmployeeStopCheckUpdateView.as_view(), name='employee-spotcheck-update'),
 ]
