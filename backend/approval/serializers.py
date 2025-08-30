e022f471667f685f312b50118f17f4dd57e69a93
prosper.kisakye <prosper.kisakye@baifamgroup.co.ug>
2025-08-30 13:58:35 +0300

    asset approvals

--- backend/approval/serializers.py
+++ backend/approval/serializers.py
@@ -30,6 +30,26 @@ class ApprovalSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ApprovalTaskSerializer(serializers.ModelSerializer):
+    level_name = serializers.CharField(source='level.name', read_only=True)
+    approval_document_description = serializers.CharField(source='approval.document.description', read_only=True)
+    content_object = serializers.SerializerMethodField()  # To show linked object info
+
     class Meta:
         model = ApprovalTask
-        fields = '__all__'
+        fields = [
+            'id', 'status', 'comment', 'approved_by', 'updated_at',
+            'level', 'level_name', 'approval_document_description',
+            'content_object'
+        ]
+        read_only_fields = ['approved_by', 'updated_at']
+
+    def get_content_object(self, obj):
+        if obj.approval.content_object:
+            # Return a dict with basic info; customize as needed
+            co = obj.approval.content_object
+            return {
+                'model': co.__class__.__name__,
+                'id': co.pk,
+                'str': str(co)
+            }
+        return None
