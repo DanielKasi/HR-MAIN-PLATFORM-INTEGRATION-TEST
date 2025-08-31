9af7ea0c189b638386ba302a35d28b29aec4d1bb
Samuel Ssenyonga <samuel.senyonga@baifamgroup.co.ug>
2025-08-31 11:24:31 +0300

    Refactor EmployeeDay model to use TimeField for start_time and end_time; add migration for RetirementRequest approval status and additional fields

--- backend/employee/models.py
+++ backend/employee/models.py
@@ -482,8 +482,8 @@ class EmployeeDay(BaseApprovableModel):
         "EmployeeWorkingDays", on_delete=models.CASCADE, related_name="employee_days"
     )
     day = models.ForeignKey("settings.SystemDay", on_delete=models.CASCADE)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
 
     def __str__(self):
         return (
