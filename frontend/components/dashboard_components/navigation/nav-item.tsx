6ac37514982a7600adf4332b9ec4f3c33b7be11f
DanielKasi <daniel.kasi@baifamgroup.co.ug>
2025-09-16 21:29:52 +0300

    Testing husky

--- frontend/components/dashboard_components/navigation/nav-item.tsx
+++ frontend/components/dashboard_components/navigation/nav-item.tsx
@@ -18,10 +18,10 @@ export const NavItemComponent = ({
 	onToggle,
 }: {
 
-	item: NavIte;
+	item: NavItem;
 	expandedItems: { [key: string]: boolean };
 	isMobileView: boolean;
-	// index: number;
+	index: number;
 	onExpand: (item_title: string) => void;
 	onToggle: () => void;
 }) => {
