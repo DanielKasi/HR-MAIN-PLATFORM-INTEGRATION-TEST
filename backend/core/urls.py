ad21a5979ff860420ad51dbeab9f35c3701c7c64
Lutaaya Jamil <jamil.lutaaya@baifamgroup.co.ug>
2025-09-17 18:51:12 +0300

    redirect

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,8 +3,10 @@ from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.conf import settings
 from django.conf.urls.static import static
+from institution.views import home
 
 urlpatterns = [
+    path("", home, name="home"),
     path("admin/", admin.site.urls),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
