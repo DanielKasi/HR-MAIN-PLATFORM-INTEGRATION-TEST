2f991f81afe236ff31d70191a5d41b38836b6b39
prosper.kisakye <prosper.kisakye@baifamgroup.co.ug>
2025-09-22 13:56:34 +0300

    fixes

--- backend/recruitment/admin.py
+++ backend/recruitment/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
-from .models import JobAdvertApplication, JobInterview, JobPosition, JobPositionAdvert, InterviewStage
+from .models import JobAdvertApplication, JobInterview, JobPosition, JobPositionAdvert, InterviewStage, SkillZoneCategory, SkillZone
 
 admin.site.register(JobPositionAdvert)
 admin.site.register(JobAdvertApplication)
 admin.site.register(JobInterview)
 admin.site.register(JobPosition)
 admin.site.register(InterviewStage)
+admin.site.register(SkillZoneCategory)
+admin.site.register(SkillZone)
 
