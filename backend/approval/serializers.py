acdcbdac74080cc1638455dfb945eee0bfb17dd9
prosper.kisakye <prosper.kisakye@baifamgroup.co.ug>
2025-09-26 14:39:04 +0300

    feat: Enhance ApprovalDocumentLevel model and serializers; add support for approver and overrider users

--- backend/approval/serializers.py
+++ backend/approval/serializers.py
@@ -90,6 +90,7 @@ class ApproverGroupSerializer(serializers.ModelSerializer):
 
 class ApprovalDocumentLevelApproverSerializer(serializers.ModelSerializer):
     approver_group = serializers.SerializerMethodField(read_only=True)
+    approver_users = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = ApprovalDocumentLevelApprovers
@@ -98,9 +99,14 @@ class ApprovalDocumentLevelApproverSerializer(serializers.ModelSerializer):
     def get_approver_group(self, obj):
         from .serializers import ApproverGroupSerializer
         return ApproverGroupSerializer(obj.approver_group, context=self.context).data
+    
+    def get_approver_user(self, obj):
+        from users.serializers import ProfileSerializer
+        return ProfileSerializer(obj.approver_group.users.all(), many=True, context=self.context).data
 
 class ApprovalDocumentLevelOverriderSerializer(serializers.ModelSerializer):
     approver_group = serializers.SerializerMethodField(read_only=True)
+    approver_users = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = ApprovalDocumentLevelOverriders
@@ -109,6 +115,10 @@ class ApprovalDocumentLevelOverriderSerializer(serializers.ModelSerializer):
     def get_approver_group(self, obj):
         from .serializers import ApproverGroupSerializer
         return ApproverGroupSerializer(obj.approver_group, context=self.context).data
+    
+    def get_approver_user(self, obj):
+        from users.serializers import ProfileSerializer
+        return ProfileSerializer(obj.approver_group.users.all(), many=True, context=self.context).data
 
 class ApprovalDocumentLevelSerializer(serializers.ModelSerializer):
     approvers = serializers.PrimaryKeyRelatedField(
